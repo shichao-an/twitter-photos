a92cbe89430fe3ff673a9cbd214bff849a81c3f0
Shichao An <shichao.an@nyu.edu>
2014-01-22 00:13:19 -0500

    Initial test cases

---
+++ twphotos/tests.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+from unittest import TestCase
+from .settings import PROJECT_PATH
+from .photos import TwitterPhotos
+
+
+TEST_OUTPUT = 'test-output'
+TEST_USER = 'WIRED'
+
+
+class TestPhotos(TestCase):
+    """Test TwitterPhotos class programmatically"""
+    def setUp(self):
+        d = os.path.join(PROJECT_PATH, TEST_OUTPUT)
+        if not os.path.exists(d):
+            os.makedirs(d)
+        self.directory = d
+
+    def test_credentials(self):
+        pass
+
+    def test_get(self):
+        twphotos = TwitterPhotos(TEST_USER)
+        p = twphotos.get(count=20)
+
+    def test_download(self):
+        twphotos = TwitterPhotos(user=TEST_USER, outdir=self.directory)
+        p = twphotos.get(count=20)
+        p = twphotos.download()
+        self.assertEqual(len(p), len(os.listdir(self.directory)))
+
+    def tearDown(self):
+        shutil.rmtree(self.directory)
