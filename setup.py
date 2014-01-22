4b86e3cd79bc8f065d73b134e7879de14d0307e9
Shichao An <shichao.an@nyu.edu>
2014-01-22 00:25:56 -0500

    Add setup.py script and update __version__ name

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from twphotos import __version__
+
+
+setup(
+    name='twitter-photos',
+    version=__version__,
+    description="Command-line tool to get photos from Twitter accounts.",
+    long_description=open('README.rst').read(),
+    keywords='twitter photos',
+    author='Shichao An',
+    author_email='shichao.an@nyu.edu',
+    url='https://github.com/shichao-an/twitter-photos',
+    license='BSD',
+    install_requires=['python-twitter', 'requests'],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'twphotos = twphotos.photos:main',
+        ],
+    },
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+    ],
+)
