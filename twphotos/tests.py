a1604a8e5fc35dc5e8b9666db1b641fc48994cdd
Shichao An <shichao.an@nyu.edu>
2014-01-31 02:36:40 -0500

    Added and improved test_increment_download

--- twphotos/tests.py
+++ twphotos/tests.py
@@ -5,7 +5,7 @@ from unittest import TestCase
 from .settings import PROJECT_PATH
 from .photos import TwitterPhotos
 from .utils import create_directory
-
+from .increment import remove_since_id
 
 TEST_OUTPUT = 'test-output'
 TEST_USER = 'WIRED'
@@ -18,14 +18,11 @@ class TestPhotos(TestCase):
         d = os.path.join(PROJECT_PATH, TEST_OUTPUT)
         create_directory(d)
         self.directory = d
+        remove_since_id(TEST_USER)
 
     def test_credentials(self):
         twphotos = TwitterPhotos()
         twphotos.verify_credentials()
-
-    def test_get(self):
-        twphotos = TwitterPhotos(TEST_USER)
-        p = twphotos.get(count=20)
 
     def test_download(self):
         twphotos = TwitterPhotos(user=TEST_USER, outdir=self.directory)
@@ -33,12 +30,29 @@ class TestPhotos(TestCase):
         twphotos.download()
         self.assertEqual(len(p), len(os.listdir(self.directory)))
 
-    def test_list_download(self):
+    def test_increment_download(self):
+        t1 = TwitterPhotos(user=TEST_USER, outdir=self.directory,
+                           num=20, increment=True)
+        photos1 = t1.get()[TEST_USER]
+        self.assertEqual(len(photos1), 20)
+        t1.download()
+        t2 = TwitterPhotos(user=TEST_USER, outdir=self.directory,
+                           num=20, increment=True)
+        photos2 = t2.get()[TEST_USER]
+        self.assertEqual(len(photos2), 0)
+        t2.download()
+
+    def _test_list_download(self):
+        """
+        Recover this test when fixed issue #138 of python-twitter
+        is released
+        """
         twphotos = TwitterPhotos(user=TEST_USER, list_slug=TEST_LIST_SLUG,
                                  outdir=self.directory)
         p = twphotos.get(count=20)
         twphotos.download()
         self.assertEqual(len(p), len(os.listdir(self.directory)))
-        
+
     def tearDown(self):
         shutil.rmtree(self.directory)
+        remove_since_id(TEST_USER)
