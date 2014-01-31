b41d06a5bd98f5bdbbf14426497a0b640eb4a9da
Shichao An <shichao.an@nyu.edu>
2014-01-31 02:57:18 -0500

    git rm --cached run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os
-from twphotos.photos import main
-
-
-PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
-config = os.path.join(PROJECT_PATH, '.twphotos')
-os.environ['TWPHOTOS_TEST_CONFIG'] = config
-main()
